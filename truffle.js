d9e9cd4dd8daf7a0c19914861b168686a7cbfe26
Julian Yap <julianokyap@gmail.com>
2021-07-28 15:18:29 -0700

    Update RPC port

--- truffle.js
+++ truffle.js
@@ -2,7 +2,7 @@ module.exports = {
   networks: {
     development: {
       host: "localhost",
-      port: 8545,
+      port: 8588,
       network_id: "*", // Match any network id
       gas: 4000000,
       gasPrice: 10000000000, // 10 gwei
