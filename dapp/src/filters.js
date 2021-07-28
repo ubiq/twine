c5b071fd8e70fec3586a16c85ed42792bc884791
Julian Yap <julianokyap@gmail.com>
2021-07-28 15:09:08 -0700

    Change ETH to UBQ units

--- dapp/src/filters.js
+++ dapp/src/filters.js
@@ -90,15 +90,15 @@
           if (casted.gt(0)) {
             var ether = casted.div('1e18');
             if (ether.gt(1)) {
-              return ether.toPrecision(Math.floor(Math.log(ether.toNumber())/Math.log(10) + 3)).toString(10) + " ETH";
+              return ether.toPrecision(Math.floor(Math.log(ether.toNumber())/Math.log(10) + 3)).toString(10) + " UBQ";
             }
             else {
-              return ether.toPrecision(2).toString() + " ETH";
+              return ether.toPrecision(2).toString() + " UBQ";
             }
 
           }
           else {
-            return "0.00 ETH";
+            return "0.00 UBQ";
           }
         }
         return null;
